22f6b68d5464bcd6adbc06f2766e31e64cd63134
PriscilaLeaoLopes <priaparecidaleao@gmail.com>
2024-06-12 14:39:54 -0300

    correção 12/06/2024

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,11 @@ export const apiCodeBurguer = axios.create({
     baseURL: 'http://localhost:3001'
 })
 
+apiCodeBurguer.interceptors.request.use(async config => {
+    const userData = await localStorage.getItem('codeburguer:userData')
+    const token = JSON.parse(userData).token
+    config.headers.authorization = 'Bearer ${token}'
+    return config
+})
+
 export default apiCodeBurguer
